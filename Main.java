a70ef3fa9ae301c80b02ea158bfed397a12cd607
younj1601 <younj1601@wrdsb.ca>
2020-10-02 16:11:49 +0000

    Finished the fourth question for 2.02

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ *Play fizz buzz with a range of numbers
+ * @author Jackson
  */
 public class Main {
 
@@ -9,7 +10,36 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    //scanner for input
+    Scanner input = new Scanner(System.in);
+
+    //get interger
+    System.out.println("Please enter a positive integer to see the FizzBuzz numbers");
+    int num = input.nextInt();
+
+    //declare and initialize count
+    int count = 0;
+
+    while(count < num){
+
+      //increase value on count by 1
+      count = count + 1;
+
+      //divisable by 5 and 3
+      if ((count % 5 == 0) && (count % 3 == 0)){
+      System.out.println("You should say FizzBuzz");
+      //divisable by 3
+    } else if ((count % 3 == 0)){
+      System.out.println("You should say Fizz");
+      //divisable by 5
+    } else if ((count % 5 == 0)){
+      System.out.println("You should say Buzz");
+      //divisable by neither
+    } else if ((count % 3 != 0) && (count % 5 != 0)){
+      System.out.println("You should say " + count);
+    }
+      
+    }
     
   }
 }
